c67e59c0534741872c424852ecba62e7dec5c31e
Yuru Gong <yurugong@Yurus-MacBook-Pro.local>
2021-05-28 23:14:48 -0400

    game logic

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -1,4 +1,6 @@
 'use strict';
+
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉Correct Number!';
 
@@ -7,3 +9,37 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20 + 1); // random() will return (0,1)
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value); // string
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '⛔️ You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '⛔️ You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});

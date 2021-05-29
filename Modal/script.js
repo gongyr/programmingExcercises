5fad084a84e8b015048334aa5ccebf7282106e48
gongyr <gongyrf@gmail.com>
2021-05-29 17:55:10 -0400

    keydown event

--- Modal/script.js
+++ Modal/script.js
@@ -11,7 +11,6 @@ const closeModal = () => {
   overlay.classList.add('hidden');
 };
 const openModal = () => {
-  console.log('Button clicked');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -22,3 +21,10 @@ for (let i = 0; i < btnOpenModal.length; i++) {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', e => {
+  console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});

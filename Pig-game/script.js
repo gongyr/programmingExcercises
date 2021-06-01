8aa0fbb847f6107b513947cfc534390345baad62
gongyr <gongyrf@gmail.com>
2021-05-31 23:03:00 -0400

    resetting the game

--- Pig-game/script.js
+++ Pig-game/script.js
@@ -13,15 +13,26 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-//Starting conditions
-score0El.textContent = '0';
-score1El.textContent = '0';
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+//Starting conditions
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = '0';
+  score1El.textContent = '0';
+  current0El.textContent = '0';
+  current1El.textContent = '0';
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
 
 const switchPlayer = () => {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -30,6 +41,9 @@ const switchPlayer = () => {
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
+
+init();
+
 //Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   if (playing) {
@@ -60,7 +74,7 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // 2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       document
         .querySelector(`.player--${activePlayer}`)
@@ -69,10 +83,11 @@ btnHold.addEventListener('click', function () {
         .querySelector(`.player--${activePlayer}`)
         .classList.remove('player--active');
     } else {
+      // Switch to the next player
       switchPlayer();
     }
     // Finish the game
     diceEl.classList.add('hidden');
-    // Switch to the next player
   }
 });
+btnNew.addEventListener('click', init);
